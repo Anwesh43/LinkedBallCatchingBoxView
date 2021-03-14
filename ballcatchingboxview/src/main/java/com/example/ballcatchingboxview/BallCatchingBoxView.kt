f1f108ca4f014df6b230bb7a3751d161d64c7457
Anwesh43 <anweshthecool0@gmail.com>
2021-03-14 17:01:26 +0530

    adding BCBNode class

--- ballcatchingboxview/src/main/java/com/example/ballcatchingboxview/BallCatchingBoxView.kt
+++ ballcatchingboxview/src/main/java/com/example/ballcatchingboxview/BallCatchingBoxView.kt
@@ -122,4 +122,45 @@ class BallCatchingBoxView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BCBNode(var i : Int, val state : State = State()) {
+
+        private var next : BCBNode? = null
+        private var prev : BCBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BCBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBCBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BCBNode {
+            var curr : BCBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
