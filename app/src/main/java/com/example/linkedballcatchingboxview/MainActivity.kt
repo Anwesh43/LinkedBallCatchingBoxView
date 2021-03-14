56ce80b27991b58196b8494a557863026ca90ba9
Anwesh43 <anweshthecool0@gmail.com>
2021-03-14 17:19:37 +0530

    adding fullscreen support

--- app/src/main/java/com/example/linkedballcatchingboxview/MainActivity.kt
+++ app/src/main/java/com/example/linkedballcatchingboxview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedballcatchingboxview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.ballcatchingboxview.BallCatchingBoxView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BallCatchingBoxView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
