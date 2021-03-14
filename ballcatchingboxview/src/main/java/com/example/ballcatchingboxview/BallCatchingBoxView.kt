3ad1a78c0e86b829c043534694235dfd37420d29
Anwesh43 <anweshthecool0@gmail.com>
2021-03-14 13:02:05 +0530

    adding Int and Float extension methods

--- ballcatchingboxview/src/main/java/com/example/ballcatchingboxview/BallCatchingBoxView.kt
+++ ballcatchingboxview/src/main/java/com/example/ballcatchingboxview/BallCatchingBoxView.kt
@@ -25,3 +25,8 @@ val strokeFactor : Float = 90f
 val parts : Int = 5
 val scGap : Float = 0.02f / parts
 val delay : Long = 20
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
